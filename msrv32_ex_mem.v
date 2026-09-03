7c43575f4016962b79eec23b00c0adf44452e856
Manu <emmanuelpunnoose@gmail.com>
2026-09-03 07:27:45 +0530

    Execute/Memory Register

---
+++ msrv32_ex_mem.v
@@ -0,0 +1,40 @@
+module msrv32_ex_mem (
+    input wire clk,
+    input wire reset,
+    input wire mem_write_in,
+    input wire mem_read_in,
+    input wire reg_write_in,
+    input wire mem_to_reg_in,
+    input wire [31:0] alu_result_in,
+    input wire [31:0] rs2_data_in,
+    input wire [4:0] rd_addr_in,
+    output reg mem_write_out,
+    output reg mem_read_out,
+    output reg reg_write_out,
+    output reg mem_to_reg_out,
+    output reg [31:0] alu_result_out,
+    output reg [31:0] rs2_data_out,
+    output reg [4:0] rd_addr_out
+);
+
+always @(posedge clk) begin
+    if (reset) begin
+        mem_write_out  <= 1'b0;
+        mem_read_out   <= 1'b0;
+        reg_write_out  <= 1'b0;
+        mem_to_reg_out <= 1'b0;
+        alu_result_out <= 32'b0;
+        rs2_data_out   <= 32'b0;
+        rd_addr_out    <= 5'b0;
+    end else begin
+        mem_write_out  <= mem_write_in;
+        mem_read_out   <= mem_read_in;
+        reg_write_out  <= reg_write_in;
+        mem_to_reg_out <= mem_to_reg_in;
+        alu_result_out <= alu_result_in;
+        rs2_data_out   <= rs2_data_in;
+        rd_addr_out    <= rd_addr_in;
+    end
+end
+
+endmodule
